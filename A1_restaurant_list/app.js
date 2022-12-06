bf0ae46096077afbe284e8d20bfd941e64df8811
LJBL22 <ljbl2208@gmail.com>
2022-12-06 19:26:38 +0800

    get & make show page content template

--- A1_restaurant_list/app.js
+++ A1_restaurant_list/app.js
@@ -19,6 +19,24 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
+//route setting _ show page
+app.get('/restaurants/1', (req, res) => {
+  const restaurantOne = {
+    "id": 1,
+    "name": "Sababa 沙巴巴中東美食",
+    "name_en": "Sababa Pita Bar",
+    "category": "中東料理",
+    "image": "https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5635/01.jpg",
+    "location": "台北市羅斯福路三段 283 巷 17 號",
+    "phone": "02 2363 8009",
+    "google_map": "https://goo.gl/maps/BJdmLuVdDbw",
+    "rating": 4.1,
+    "description": "沙巴巴批塔是台灣第一家純手工批塔專賣店,只選用最新鮮的頂級原料,以及道地的中東家傳配方。"
+  }
+
+  res.render('show', { restaurants: restaurantOne })
+})
+
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
